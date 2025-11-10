06d6829daa57467c5b01058bdcdb13ae52103c5b
이예니 <126794376+LeeYeni@users.noreply.github.com>
2025-11-10 10:23:28 +0900

    fix: FastAPI 엔드포인트가 status를 포함한 JSON을 반환하도록 수정

--- main.py
+++ main.py
@@ -12,21 +12,38 @@ def root() -> dict:
 #---------- 1. 데이터 불러오기 & 전처리 ----------#
 @api.get("/data_processor")
 def data_processor(file_path: str) -> dict:
-    data_handler = DataHandler(file_path)
-    coords = data_handler.processor()
-    return coords
+    try:
+        data_handler = DataHandler(file_path)
+        coords = data_handler.processor()
+        return {
+            "status": "success",
+            "coords": coords,
+        }
+    
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
 
 #---------- 2. 카카오맵 '즐겨찾기 추가' 기능 수행 ----------#
 @api.post("/kakao_map_task")
 def kakao_map_task(data: dict) -> dict:
-    kakao_map = KakaoMap()
+    try:
+        kakao_map = KakaoMap()
 
-    while True:
-        if kakao_map.is_login():  # 로그인
-            break
+        while True:
+            if kakao_map.is_login():  # 로그인
+                break
 
-    kakao_map.click("skyview")  # 스카이뷰
-    kakao_map.search_and_save(data["coords"], data["folder_name"])  # 즐겨찾기 추가
-    kakao_map.driver.quit()  # 브라우저 종료
+        kakao_map.click("skyview")  # 스카이뷰
+        kakao_map.search_and_save(data["coords"], data["folder_name"])  # 즐겨찾기 추가
+        kakao_map.driver.quit()  # 브라우저 종료
 
-    return {"status": "success"}
+        return {"status": "success"}
+    
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
